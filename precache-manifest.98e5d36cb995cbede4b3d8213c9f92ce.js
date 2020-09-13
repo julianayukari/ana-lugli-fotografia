c2262f1464682492ef3a3705da3c1d60db84e319
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2020-09-13 20:08:44 -0300

    Updates

---
+++ precache-manifest.98e5d36cb995cbede4b3d8213c9f92ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce3c12ed9351cd1ceff90aa843773a8",
+    "url": "/ana-lugli-fotografia/index.html"
+  },
+  {
+    "revision": "a1c87eaca24fc847de30",
+    "url": "/ana-lugli-fotografia/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a1c87eaca24fc847de30",
+    "url": "/ana-lugli-fotografia/static/js/2.54a96c06.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/ana-lugli-fotografia/static/js/2.54a96c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2060ad93fcd708f51fb9",
+    "url": "/ana-lugli-fotografia/static/js/main.8cf3838d.chunk.js"
+  },
+  {
+    "revision": "85fd7829f17d6a6bcad9",
+    "url": "/ana-lugli-fotografia/static/js/runtime-main.4362b8b9.js"
+  }
+]);
